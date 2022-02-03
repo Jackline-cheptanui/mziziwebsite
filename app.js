450b85db6fffdd4cbc5c5fc754068779a35d8109
jamiecode <jamesgituma9961@gmail.com>
2022-02-03 13:27:30 +0300

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
@@ -11,12 +12,13 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
+	if (scroll_position > 200) {
 		header.style.backgroundColor = '#29323c';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
 });
+
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
